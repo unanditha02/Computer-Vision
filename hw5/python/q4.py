313e98fe25a195279fd40ecc20f5d4d4c5b2d107
Nanditha Umasankar <unanditha02@gmail.com>
2021-11-23 13:29:59 -0500

    q4

--- hw5/python/q4.py
+++ hw5/python/q4.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from skimage import data
 import skimage
 import skimage.measure
 import skimage.color
@@ -7,6 +7,8 @@ import skimage.restoration
 import skimage.filters
 import skimage.morphology
 import skimage.segmentation
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 # takes a color image
 # returns a list of bounding boxes and black_and_white image
@@ -16,9 +18,37 @@ def findLetters(image):
     # insert processing in here
     # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes 
     # this can be 10 to 15 lines of code using skimage functions
-
+    image = skimage.color.rgb2gray(image)
     ##########################
     ##### your code here #####
     ##########################
+    thresh = skimage.filters.threshold_otsu(image)
+    bw = skimage.morphology.closing(image < thresh, skimage.morphology.square(10))
 
+    # remove artifacts connected to image border
+    cleared = skimage.segmentation.clear_border(bw)
+
+    # label image regions
+    label_image = skimage.measure.label(cleared)
+
+    # to make the background transparent, pass the value of `bg_label`,
+    # and leave `bg_color` as `None` and `kind` as `overlay`
+    image_label_overlay = skimage.color.label2rgb(label_image, image=image, bg_label=0)
+
+    for region in skimage.measure.regionprops(label_image):
+        if region.area >= 100:
+            bboxes.append(region.bbox)
+  
+    #     # take regions with large enough areas
+    #     if region.area >= 100:
+    #         # draw rectangle around segmented coins
+    #         minr, minc, maxr, maxc = region.bbox
+    #         rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+    #                                 fill=False, edgecolor='red', linewidth=2)
+    #         ax.add_patch(rect)
+
+    # ax.set_axis_off()
+    # plt.tight_layout()
+    # plt.show()
+    bw = (~bw).astype(np.float)
     return bboxes, bw
--- hw5/python/run_q4.py
+++ hw5/python/run_q4.py
@@ -11,7 +11,8 @@ import skimage.io
 import skimage.filters
 import skimage.morphology
 import skimage.segmentation
-
+import pickle
+import string
 from nn import *
 from q4 import *
 # do not include any more libraries here!
@@ -23,34 +24,84 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 for img in os.listdir('../images'):
     im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
     bboxes, bw = findLetters(im1)
-
-    plt.imshow(bw)
+    
+    plt.imshow(bw, cmap='gray')
     for bbox in bboxes:
         minr, minc, maxr, maxc = bbox
         rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                 fill=False, edgecolor='red', linewidth=2)
         plt.gca().add_patch(rect)
     plt.show()
-    # find the rows using..RANSAC, counting, clustering, etc.
-    ##########################
-    ##### your code here #####
-    ##########################
 
-
+    # # find the rows using..RANSAC, counting, clustering, etc.
+    # ##########################
+    # ##### your code here #####
+    # ##########################
+    
     # crop the bounding boxes
     # note.. before you flatten, transpose the image (that's how the dataset is!)
     # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
     ##########################
     ##### your code here #####
     ##########################
+    heights = [bbox[2]-bbox[0] for bbox in bboxes]
+    mean_height = sum(heights)/len(heights)
+        # sort the bounding boxes with center y
+    centers = [((bbox[2]+bbox[0])//2, (bbox[3]+bbox[1])//2, bbox[2]-bbox[0], bbox[3]-bbox[1]) for bbox in bboxes]
+    centers = sorted(centers, key=lambda p: p[0])
+    rows = []
+    pre_h = centers[0][0]
+        # cluster rows
+    row = []
+    for c in centers:
+        if c[0] > pre_h + mean_height:
+                row = sorted(row, key=lambda p: p[1])
+                rows.append(row)
+                row = [c]
+                pre_h = c[0]
+        else:
+                row.append(c)
+    row = sorted(row, key=lambda p: p[1])
+    rows.append(row)
+
+    data = []
+    for row in rows:
+        row_data = []
+        bbox_row = np.array(row)
+        for bbox in bbox_row:
+            # crop out the character
+            y, x, h, w = bbox
+            crop = bw[y-h//2:y+h//2, x-w//2:x+w//2]
+                # pad it to square
+            h_pad, w_pad = 0, 0
+            if h > w:
+                    h_pad = h//20
+                    w_pad = (h-w)//2+h_pad
+            elif h < w:
+                    w_pad = w//20
+                    h_pad = (w-h)//2+w_pad
+            crop = np.pad(crop, ((h_pad, h_pad), (w_pad, w_pad)), 'constant', constant_values=(1, 1))
+            crop = skimage.transform.resize(crop, (32, 32), anti_aliasing=False)
+            crop = np.transpose(crop)
+            # plt.imshow(crop, cmap='gray')
+            # plt.show()
+            row_data.append(crop.flatten())
+        data.append(np.array(row_data))
     
     # load the weights
     # run the crops through your neural network and print them out
-    import pickle
-    import string
+    
     letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
     params = pickle.load(open('q3_weights.pickle','rb'))
     ##########################
     ##### your code here #####
     ##########################
-    
+    pred = []
+
+    for i in range(len(data)):
+        h1 = forward(data[i], params, 'layer1')
+        probs = forward(h1, params, 'output',softmax)
+        for j in range(len(data[i])):
+            pred.append(letters[np.argmax(probs[j,:])])
+    print("Image ", img)
+    print(pred)
